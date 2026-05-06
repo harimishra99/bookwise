139c7c80dc3c09f71b05e852b0fa448404938a96
Hari Shankar Mishra <harimishra.professional@gmail.com>
2026-05-06 23:47:02 +0530

    JMD1

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -114,7 +114,7 @@ DATABASES = {
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT', '6543'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
         'OPTIONS': {
             'sslmode': os.environ.get('DB_SSLMODE', 'require')
         }
